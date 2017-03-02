eee64f09d5fe715d8c65d9a5a07f0a602978fed6
tinico33 <ploquin.nicolas@gmail.com>
2017-03-02 13:33:50 +0100

    Suppression du dossier node_modules du projet et mise à jour du package.json

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 var express     = require('express');
 var path        = require('path');
-var logger      = require('morgan');
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
- 
+
 var app = express();
  
-app.use(logger('dev'));
 app.use(bodyParser.json());
 
 var mongodbUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/booking';
